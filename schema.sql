03c1b8c7a55299394c8e4dbf1e1426714784022b
Mónica Salvatierra <sal22249@uvg.edu.gt>
2024-04-25 00:06:54 -0600

    Creación de tablas de la base de datos

---
+++ schema.sql
@@ -0,0 +1,35 @@
+
+--Creación de tablas--
+CREATE TABLE IF NOT EXISTS Clientes (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    direccion VARCHAR(100),
+    codigoAcceso VARCHAR(100),
+    telefono VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS Inventario (
+    id SERIAL PRIMARY KEY,
+    producto VARCHAR(100),
+    marca VARCHAR(100),
+    valor INTEGER,
+    precio FLOAT,
+    cantidad INTEGER,
+    comentarios TEXT
+);
+
+CREATE TABLE IF NOT EXISTS pedidos (
+    id_pedido SERIAL PRIMARY KEY,
+    id_cliente INTEGER NOT NULL,
+    comentarios TEXT,
+    descripcion_pedido TEXT,
+    fecha_entrega DATE,
+    fecha_recoger DATE,
+    estado VARCHAR(20),
+    total FLOAT
+    FOREIGN KEY (id_cliente) REFERENCES Clientes(id)
+);
+
+
+
